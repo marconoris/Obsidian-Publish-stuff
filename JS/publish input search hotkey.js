c02a3efd0b40abd703db2ee11d43f0d32ef2c565
zarconoris <chewzer@gmail.com>
2024-03-20 12:09:57 +0100

    Update and rename Select input search with keyboard shortcut.js to publish input search hotkey.js

--- JS/publish input search hotkey.js
+++ JS/publish input search hotkey.js
@@ -1,3 +1,9 @@
+/* 
+Marco Noris - https://lab.marconoris.com 
+Select the Obsidian Publihs input search by pressing "ctrl+f" keys
+*/
+
+
 function setupKeyboardShortcut() {
   document.addEventListener('keydown', function(e) {
     if (e.ctrlKey && e.key === 'f') { //It is possible to change with other characters.
