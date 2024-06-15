ffdd38a86a6fb1f605b11b99d480f1b1d7a1dab5
zarconoris <chewzer@gmail.com>
2024-06-15 11:57:07 +0200

    Update toggle Obsidian Publish sidebars.js
    This version of the script enable you to use `url+?reader=true` to open a page in reader view.

--- JS/toggle Obsidian Publish sidebars.js
+++ JS/toggle Obsidian Publish sidebars.js
@@ -1,10 +1,7 @@
-/* 
-Marco Noris - https://lab.marconoris.com 
-*/
-
 // START HIDE SIDEBARS 
 
 // Use the “r” key to activate the reading view
+// Use url+?reader=true to open a page with reader view. Example: https://lab.marconoris.com/obsidian?reader=true
 
 // Iconos SVG para los estados visible y oculto
 const iconVisible = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-maximize-2"><polyline points="15 3 21 3 21 9"/><polyline points="9 21 3 21 3 15"/><line x1="21" x2="14" y1="3" y2="10"/><line x1="3" x2="10" y1="21" y2="14"/></svg>`; // SVG para cuando las sidebars están visibles
@@ -24,15 +21,24 @@ function toggleSidebarsAndFooter() {
     leftSidebar.style.display = isHidden ? '' : 'none';
     siteFooter.style.display = isHidden ? '' : 'none';
     
-    // Alternar la clase 'header-visible' del .site-header
+    // Alternar la clase 'reader-view' del .site-header
     if (isHidden) {
-      siteHeader.classList.remove('header-visible');
+      siteHeader.classList.remove('reader-view');
     } else {
-      siteHeader.classList.add('header-visible');
+      siteHeader.classList.add('reader-view');
     }
 
     // Cambia el icono del botón según el estado de las sidebars
     toggleButton.innerHTML = isHidden ? iconVisible : iconHidden;
+
+    // Guardar el estado en localStorage
+    localStorage.setItem('sidebarsHidden', !isHidden);
+
+    // Actualizar la URL con el parámetro de consulta
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('reader', !isHidden);
+    const newUrl = `${window.location.pathname}?${urlParams.toString()}`;
+    window.history.replaceState(null, '', newUrl);
   }
 }
 
@@ -54,12 +60,24 @@ function applyInitialVisibilityState() {
   const siteHeader = document.querySelector('.site-header');
   const toggleButton = document.querySelector('#toggle-sidebar-btn');
 
-  // Asumir que las sidebars deben estar visibles inicialmente
-  rightSidebar.style.display = '';
-  leftSidebar.style.display = '';
-  siteFooter.style.display = '';
-  siteHeader.classList.remove('header-visible');
-  toggleButton.innerHTML = iconVisible;
+  // Obtener el estado de visibilidad de localStorage
+  let sidebarsHidden = localStorage.getItem('sidebarsHidden') === 'true';
+
+  // Verificar el parámetro de consulta en la URL
+  const urlParams = new URLSearchParams(window.location.search);
+  if (urlParams.has('reader')) {
+    sidebarsHidden = urlParams.get('reader') === 'true';
+  }
+
+  rightSidebar.style.display = sidebarsHidden ? 'none' : '';
+  leftSidebar.style.display = sidebarsHidden ? 'none' : '';
+  siteFooter.style.display = sidebarsHidden ? 'none' : '';
+  if (sidebarsHidden) {
+    siteHeader.classList.add('reader-view');
+  } else {
+    siteHeader.classList.remove('reader-view');
+  }
+  toggleButton.innerHTML = sidebarsHidden ? iconHidden : iconVisible;
 }
 
 // Función para crear y añadir el botón de alternar al .site-body-center-column
@@ -101,4 +119,5 @@ function waitForSiteBodyCenterColumn() {
 // Ejecuta la función waitForSiteBodyCenterColumn cuando el script se carga
 waitForSiteBodyCenterColumn();
 
-// END HIDE SIDEBARS 
+// END HIDE SIDEBARS
+
